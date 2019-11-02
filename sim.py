bb655d464f0f56ebd91deb5ee5e6589ab6cb48bc
pyr <bjelic.ilija@yahoo.com>
2019-11-02 13:19:06 +0100

    simulation

--- sim.py
+++ sim.py
@@ -2,17 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 from matplotlib.animation import FuncAnimation
-import math, time
+import math, time, matplotlib, random
 
-dt = 1e-3 #second
+
+#Some important variables
+dt = 1e-2 #second
 #assume that all distance units are in meters
-T=2#time of simulation in seconds
-animation_speed=10 #how much times faster should animation be
+T=10#time of simulation in seconds
+animation_speed=3 #how much times faster should animation be
 e0 = 8.85e-12; #epsilon 0 
 gravity_constant = 6.67e-11; #gravity constant
+
+#For animation
 fig = plt.figure() #plot
-ax1 = fig.add_subplot(1,2,1)
-ax2 = fig.add_subplot(1,2,2)
+ax1 = fig.add_subplot(1,2,1) #graph for bodies movment
+ax2 = fig.add_subplot(1,2,2) #graph velocities=f(time)
 
 class body:
 	def __init__(self, x, y, m, q, r, vx, vy):
@@ -28,7 +32,8 @@ class body:
 	def angle(self, body): #angle between this and other body in radians
 		x_dist = self.x-body.x; #one side of right angle triangle
 		y_dist = self.y-body.y; #second side of same triangle
-		if(x_dist==0 and y_dist<0):
+		#prevent division by 0
+		if(x_dist==0 and y_dist<0): 
 			angle=3.14/2
 		elif(x_dist==0 and y_dist>0):
 			angle=-3.14/2
@@ -36,8 +41,8 @@ class body:
 			angle = np.arctan(y_dist/x_dist); #division of sides gives tan(angle)
 		return angle
     
-	def dist(self,body):
-		dis = math.sqrt((self.x-body.x)**2+(self.y-body.y)**2) #distance between two points
+	def dist(self,body): #distance between two points (self and other body)
+		dis = math.sqrt((self.x-body.x)**2+(self.y-body.y)**2) 
 		return dis
     
 	def force(self, body): #calculate force on body
@@ -48,7 +53,6 @@ class body:
 				self.f[0]=self.f[0]+1/(4*3.14*e0)*self.q*body[i].q/distance**2*np.sin(self.angle(body[i])) #electric force x
 				self.f[1]=self.f[1]+gravity_constant*self.m*body[i].m/distance**2*np.cos(self.angle(body[i])) #gravity force y
 				self.f[1]=self.f[1]+1/(4*3.14*e0)*self.q*body[i].q/distance**2*np.cos(self.angle(body[i])) #electric force y
-		
 		return self.f
 	
 	def acceleration(self):
@@ -80,8 +84,6 @@ class body:
 				while(self.dist(body[i])<=(self.r/2+body[i].r/2)): #prevent another body to detect collision
 					self.move()
 					body[i].move()
-				print(distance)
-				print(self.x,body[i].x)
 	def move(self):
 		self.x=self.x+self.v[0]*dt #move for time step x
 		self.y=self.y+self.v[1]*dt #move for time step y
@@ -95,7 +97,18 @@ def simulation(bodies, t):
 	print("%.0f iterations" % N)
 	simulation_matrix = np.zeros([N,len(bodies),5])
 	for k in range(N):
-		if(k==int(N/4)):  #debugging info 
+		for i in range(len(bodies)):
+			bodies[i].force(bodies) #calculate force on each body
+			bodies[i].collision(bodies) #check for collisions
+			bodies[i].acceleration() #calculate new velocity for each body
+			bodies[i].move() #move all bodies for time step
+			#add positions and velocities into one matrix 
+			simulation_matrix[k][i][0], simulation_matrix[k][i][1] = bodies[i].x, bodies[i].y
+			simulation_matrix[k][i][2], simulation_matrix[k][i][3] = bodies[i].v[0], bodies[i].v[1]
+			simulation_matrix[k][i][4] = bodies[i].r
+		
+		#proogress info
+		if(k==int(N/4)):  
 			end_ = time.time()
 			print("25%s done, ETA = %.3f s, Time in simulation: %.3f" %("%",(end_-start)*3,k*dt)) 
 		if(k==int(N/2)): 
@@ -104,22 +117,28 @@ def simulation(bodies, t):
 		if(k==int(N*3/4)):
 			end_ = time.time()
 			print("75%s done, ETA = %.3f s, Time in simulation: %.3f" %("%",(end_-start)/4,k*dt))
-		for i in range(len(bodies)):
-			bodies[i].force(bodies) #calculate force on each body
-			bodies[i].collision(bodies) #check for collisions
-			bodies[i].acceleration() #calculate new velocity for each body
-			bodies[i].move() #move all bodies for time step
-			simulation_matrix[k][i][0], simulation_matrix[k][i][1] = bodies[i].x, bodies[i].y
-			simulation_matrix[k][i][2], simulation_matrix[k][i][3] = bodies[i].v[0], bodies[i].v[1]
-			simulation_matrix[k][i][4] = bodies[i].r
+		if(k==0): 
+			end_ = time.time()
+			print('Time for one iteration: %.3f seconds, ETA: %.3f seconds' %(end_-start,(end_-start)*N))
+		
 	end = time.time()
 	print("100%s done, time taken to simulate: %.3f seconds" % ("%",end-start))
-	return simulation_matrix
+	return simulation_matrix #get back the data
 
-test = [body(0,10.001, 20,1e-4,5,0,-10),body(0,0,1,1e-5,5,0,0),body(50,100,1,-1e-2,5,-10,-20) ]
+#generate random coordinates
+test = []
+number_of_bodies = 10
+xcor, ycor = random.sample(range(100), number_of_bodies), random.sample(range(100), number_of_bodies)
+mass, charge = random.sample(range(50),number_of_bodies), random.sample(range(-50,50), number_of_bodies)
+for i in range(number_of_bodies):
+	test.append(body(xcor[i],ycor[i],mass[i],charge[i]*1e-1,1,0,0))
+
+#simulation start
 print("Starting simulation with %.0f bodies for time period of %.2f seconds" %(len(test),T))
+simulation_data = simulation(test,T) 
 
-simulation_data = simulation(test,T) #simulation start
+
+#Create animation
 colors = cm.rainbow(np.linspace(0, 1, len(test))) #create colors list 
 
 def update(frame):	#animation frames update
@@ -136,6 +155,6 @@ def update(frame):	#animation frames update
 
 
 
-animation = FuncAnimation(fig, update, interval=1, frames=int(len(simulation_data)/animation_speed), repeat=False)
+animation = FuncAnimation(fig, update, interval=44, frames=int(len(simulation_data)/animation_speed), repeat=False)
 plt.show()
 
